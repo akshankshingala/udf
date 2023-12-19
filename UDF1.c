17972677fcdf232c5f815fd0b2298cfaf6eb6ce0
Harsh-Mangukiya <singalaakshank98@gmail.com>
2023-12-19 17:30:11 +0530

    udf........

---
+++ UDF1.c
@@ -0,0 +1,33 @@
+/*Q.1 Write a Program to find the cube of a given number using UDF.For example,
+Input:
+Enter any number: 5
+
+Output:
+Cube is: 125
+
+Q.2 Write a Program to check if a given number is divisible by both 3 & 5 or not using UDF.For example,
+Input:
+Enter any number: 15
+
+Output:
+The given number is divisible by both 3 & 5.
+*/
+
+#include<stdio.h>
+	
+	void ak (){
+	
+	int a,b;
+	printf("enter any value A : ");
+	scanf("%d", &a);
+	
+	b=a*a*a;
+	printf(" ans = %d ",b);
+	
+	
+}
+	main(){
+		ak();
+	}
+	
+
